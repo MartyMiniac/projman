71b6025c1f096fc96bb7d28e0658dfc8043ce335
Rohan Verma <rohanvermaara@gmail.com>
2023-03-04 11:21:07 +0530

    updated build file and added clean file

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
+javac projman/Main.java
 jar cvfe projman.jar ^
 projman.Main projman/*.class ^
 projman/stuctures/*.class ^
 projman/helpers/*.class ^
 projman/controllers/*.class ^
 projman/commands/*.class
+call clean.bat
---
+++ clean.bat
@@ -0,0 +1,5 @@
+del projman\*.class ^
+projman\stuctures\*.class ^
+projman\helpers\*.class ^
+projman\controllers\*.class ^
+projman\commands\*.class
